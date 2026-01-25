b1d84f7c709d24c73de72fc6a5e8f2a86eaf9181
Pawan <94308855+bEinG-RudY@users.noreply.github.com>
2026-01-25 23:58:48 +0530

    ...

--- FlappyBird.java
+++ FlappyBird.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-
 public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     int boardWidth = 360;
     int boardHeight = 640;
